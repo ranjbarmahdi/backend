7d052b173368a831a80108fac5688467d33998cd
ranjbarmahdi <ranmahdi66@gmail.com>
2025-02-12 15:44:47 +0330

    A

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 import express from 'express';
 import sequelize from './config/sequelize.config.js';
 import models from './models/initial.js';
-// import connectToChannel from './config/rabbitmq.config.js';
-// import initialConsumers from './consumers/initial.js';
+import connectToChannel from './config/rabbitmq.config.js';
+import initialConsumers from './consumers/initial.js';
 import errorHandler from './middlewares/errorHandler.js';
 import responseFormatter from './middlewares/responseFormatter.js';
 
@@ -15,8 +15,8 @@ app.use(responseFormatter);
     await sequelize.sync({ alert: true });
     console.log('initualize');
 
-    // const channel = await connectToChannel();
-    // await initialConsumers(channel);
+    const channel = await connectToChannel();
+    await initialConsumers(channel);
 })();
 
 app.use(errorHandler);
